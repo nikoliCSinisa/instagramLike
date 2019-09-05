ef726926a74ae32845dcb88d80f8bb4840d93988
Nicos <nikolicsinisa@outlook.com>
2019-09-05 14:30:22 +0200

    Kreiranje i upravljanje postovima

--- instagramLike/app/Post.php
+++ instagramLike/app/Post.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    //Pass throught validated data into db
+
+    protected $guarded = [];
+
     //Define link beetween post and user who post it
 
     public function user(){
